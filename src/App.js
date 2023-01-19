da4e675e9cfd5d911165859a77b3c69af10925bd
hWeitian <huangweitian1@hotmail.com>
2023-01-19 12:22:57 +0800

    install MUI and change default font

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Baloo 2', cursive;",
+  },
+});
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Typography>
+              Edit <code>src/App.js</code> and save to reload.
+            </Typography>
+          </header>
+        </div>
+      </ThemeProvider>
     );
   }
 }
